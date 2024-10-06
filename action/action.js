cc833fca0769117aacb27f2d7f9a9a89789aba48
hinatango <115511706+hinatango@users.noreply.github.com>
2024-10-07 08:44:22 +0900

    Add files via upload

--- action/action.js
+++ action/action.js
@@ -2,13 +2,21 @@ function exploreArea(area) {
     if (confirm(area + "に探索に出かけますがよろしいですか？")) {
         let itemsFound = ["ハンマー", "木片", "くぎ", "のこぎり"];
         let numItems = Math.floor(Math.random() * 2) + 2;
-        let foundItems = [];
+        let foundItems = {};
         for (let i = 0; i < numItems; i++) {
-            foundItems.push(itemsFound[Math.floor(Math.random() * itemsFound.length)]);
+            let item = itemsFound[Math.floor(Math.random() * itemsFound.length)];
+            foundItems[item] = (foundItems[item] || 0) + 1;
         }
-        for (let item of foundItems) {
-            logMessage(item + "を獲得しました。");
+        
+        let itemList = document.getElementById("item-list");
+        for (let item in foundItems) {
+            logMessage(item + "を" + foundItems[item] + "個獲得しました。");
+            let listItem = document.createElement("li");
+            listItem.textContent = item + " (" + foundItems[item] + ")";
+            itemList.appendChild(listItem);
         }
-        alert(area + "を探索しました！");
+        
+        alert(area + "の探索が終わりました！獲得アイテムはアイテム袋にしまいました。\n＜獲得アイテム＞\n" +
+              Object.entries(foundItems).map(([key, value]) => "・" + key + "(" + value + ")").join("\n"));
     }
 }
--- main/main.js
+++ main/main.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("ゲームがロードされました");
+    alert("このゲームでは、実際に存在するたくさんの職業の仕事についてゲーム感覚で学ぶことを目的としたゲームです。全て無料で遊べますが、一部有料アイテムが存在しますので、ご了承いただきますようお願いします。");
 });
 
 function openBag() {
-    alert("アイテム袋の機能は現在改装中です");
+    const itemBag = document.getElementById("item-bag");
+    itemBag.classList.toggle("hidden");
 }
 
 function showShop() {
@@ -16,6 +17,14 @@ function showAiRobotManagement() {
 
 function showFacilityRestoration() {
     alert("施設復興所は現在改装中です");
+}
+
+function showPokedex() {
+    alert("図鑑は現在改装中です");
+}
+
+function showHelp() {
+    alert("説明書：\n・このゲームは、より多くの職業ロボを開放し、職業について詳しくなることを目的としたゲームです。\n・エリアを探索してアイテムを集めると、職業ロボが解放されていきます。\n・職業ロボを開放していくと、探索するエリアが増え、拾えるアイテムの種類も増えていきます。\n・職業を開放していくと、施設を開放して街の復興ができるようになります。\n・職業ロボの開放、施設の開放を目指して頑張ってください！");
 }
 
 function debugMode() {
